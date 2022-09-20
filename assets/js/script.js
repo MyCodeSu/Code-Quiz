24dee0c51d46ff586b41e067d8c9d3ec67673d9b
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-09-19 21:15:33 -0400

    fixed countdown function

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,22 +56,24 @@ var quizContent = [
 // timer function
 
 function countdown() {
-const startingMinutes = 2;
-let time = startingMinutes * 60;
+let seconds = 120;
 
 const countdownEl = document.getElementById('countdown');
 
 setInterval(updateCountdown, 1000);
 
+
+
 function updateCountdown() {
-
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-
-seconds = seconds < 2 ? '0' + seconds : seconds;
-
-    countdownEl.innerHTML = `${minutes} : ${seconds}`
-    time--;
+    if (seconds <= 0) {
+        clearInterval(seconds);
+        countdownEl.innerHTML = "Time's up!";
+    } else if ( quizContent === quizContent.length ) {
+        clearInterval(seconds);
+    } else {
+        countdownEl.innerHTML = `Time remaining: ${seconds} seconds`
+    seconds--;
+    }
 }
 }
 
